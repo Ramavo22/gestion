09cdd126bec1b860cfce89e9b0a29f0545aaef65
Ramavo Harinaivo <nyxrafaly@gmail.com>
2024-09-26 23:06:20 +0300

    Ajout de controller

--- src/main/java/mg/itu/gestion/GestionApplication.java
+++ src/main/java/mg/itu/gestion/GestionApplication.java
@@ -18,16 +18,16 @@ public class GestionApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(GestionApplication.class, args);
 
-		ChargeService chargeService = context.getBean(ChargeService.class);
+		// ChargeService chargeService = context.getBean(ChargeService.class);
 
 
-		Integer rubriqueId = 2;
-		Double montantTotal = 1000000.0;
-		Short unityId = 1;
-		Date d = Date.valueOf("2024-09-26");
+		// Integer rubriqueId = 2;
+		// Double montantTotal = 1000000.0;
+		// Short unityId = 1;
+		// Date d = Date.valueOf("2024-09-26");
 
-		chargeService.save(rubriqueId, montantTotal, unityId, d);
-		System.out.println("nice");
+		// chargeService.save(rubriqueId, montantTotal, unityId, d);
+		// System.out.println("nice");
 
 		
 
--- src/main/java/mg/itu/gestion/controller/CentreController.java
+++ src/main/java/mg/itu/gestion/controller/CentreController.java
@@ -1,8 +1,24 @@
 package mg.itu.gestion.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import mg.itu.gestion.entity.Centre;
+import mg.itu.gestion.service.CentreService;
+
 @RestController
+@RequestMapping("api/Centre")
 public class CentreController {
-    
+
+    @Autowired
+    CentreService centreService;
+
+    @GetMapping("/list")
+    List<Centre> getListCentres(){
+        return centreService.findAll();
+    }
 }
